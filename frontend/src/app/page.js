5f1b7a402e02a3fbec9ff19a7b0333a8a7c6dd7b
Mags070 <pranav.diwase@gmail.com>
2026-01-09 15:37:32 +0530

    linking of sign-up sing in page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,13 @@
 import Image from "next/image";
-import ProductDetails from "@/app/product/productDetails"
-import Login from "@/app/sign-in/login"
-import Signup from "./sign-in/signUp";
+import ProductDetails from "@/app/product/page"
+import Login from "@/app/sign-in/login/page"
+import Signup from "./sign-in/signup/page";
 import Navbar from "@/components/navbar/navbar"
 export default function Home() {
   return (
    <>
    <div className="bg-[#FFF9EF] h-screen w-screen">
-    <Navbar/>
-   <ProductDetails/>
+    <Login/>
    </div>
    </>
   );
